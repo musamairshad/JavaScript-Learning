c8e54a3aa6da9d3264e2a074d49ad73d2bbddb41
Muhammad Usama <imusamairshad@gmail.com>
2024-02-09 15:15:21 +0500

    index.js is added.

---
+++ index.js
@@ -0,0 +1,44 @@
+
+// Data types.
+// alert("Hello, World!");
+// alert(2 + 3);
+// alert(typeof(23));
+// alert(typeof("Hello"));
+// alert(typeof(true));
+
+// Concatenation.
+// prompt("What is your name?");
+// var myName = "Usama";
+// // alert(myName);
+// var yourName = prompt("What is your name?");
+// alert("My name is " + myName + ", welcome to my course " + yourName + "!");
+
+// var gameLevel = 1;
+// gameLevel = 2;
+// gameLevel = 3;
+// alert("Your level is currently: " + gameLevel);
+
+// var a = 3;
+// var b = 8;
+
+// // Variable swaping.
+// var c = a;
+// a = b;
+// b = c;
+// alert("a is " + a + " & " + "b is " + b);
+
+
+// Variable naming styles.
+// var myName = "Usama";
+// var yourName = "Ahmed";
+// var my123 = 123;
+// var userScoreFinal = 12; // CamelCase.
+
+// alert("Hello" + " " + "World");
+// alert("Hello " + " World");
+
+
+// String Concatenation.
+// var messege = "Hello";
+// var name = prompt("What is your name?");
+// alert(messege + " there, " + name);
