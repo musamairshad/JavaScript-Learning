326c7f2f01e1a0b652b0cc4516dd6bc6c4c7e968
Muhammad Usama <imusamairshad@gmail.com>
2024-02-12 13:01:55 +0500

    modified.

--- index.js
+++ index.js
@@ -217,3 +217,30 @@
 //     turnRight();
 //  }
 
+
+// function getMilk(bottles) {   
+//     console.log("leaveHouse");
+//     console.log("moveRight");
+//     console.log("moveRight");
+//     console.log("moveUp");
+//     console.log("moveUp");
+//     console.log("moveUp");
+//     console.log("moveUp");
+//     console.log("moveRight");
+//     console.log("moveRight");
+//     console.log("buy " + bottles + " bottles of milk."); // bottles = 12
+//     var cost = bottles * 1.5;
+        //Do something with cost.
+//     console.log("moveLeft");
+//     console.log("moveLeft");
+//     console.log("moveDown");
+//     console.log("moveDown");
+//     console.log("moveDown");
+//     console.log("moveDown");
+//     console.log("moveLeft");
+//     console.log("moveLeft");
+//     console.log("enterHouse");
+//   }
+  
+//   getMilk(12);
+
