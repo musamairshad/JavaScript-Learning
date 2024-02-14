f5c8455c79784c8d8b4dc245dd5bed91d2fe5d06
Muhammad Usama <imusamairshad@gmail.com>
2024-02-14 13:00:08 +0500

    modified.

--- javascript_intermediate.js
+++ javascript_intermediate.js
@@ -6,3 +6,47 @@
 // n = Math.floor(n) + 1;
 // console.log(n);
 
+
+// // Love Calculator.
+// alert("Welcome to The Love Calculator: ");
+// var firstPerson = prompt("Enter the name of 1st person: ");
+// var secondPerson = prompt("Enter the name of 2nd person: ");
+
+// var n = Math.random();
+// n = n * 100;
+// n = Math.floor(n) + 1;
+
+// if(n > 70) {
+//     alert("Your love score is " + n + " %" + " You love each other like Kanye loves Kanye.");
+// }
+
+// if (n > 30 && n <= 70) {
+//     alert("The Love Score between " + firstPerson + " & " + secondPerson + " is " + n + " %");
+// }
+
+// if (n <= 30) {
+//     alert("The Love Score between " + firstPerson + " & " + secondPerson + " is " + n + " %" + " You go together like oil and water.");
+// }
+
+// else {
+//     alert("Your love score is " + n);
+// }
+
+
+// var a = 1;
+// var b = "1";
+// if (a === b) {
+//     console.log("Yes.");
+// } else {
+//     console.log("No."); // This one executes.
+// }
+
+
+// var a = 1;
+// var b = "1";
+// if (a == b) {
+//     console.log("Yes."); // This one executes.
+// } else {
+//     console.log("No.");
+// }
+
