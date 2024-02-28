f290d30c26b9bdff884b227a32f91c557f1ac3e4
Muhammad Usama <imusamairshad@gmail.com>
2024-02-28 15:38:18 +0500

    modified.

--- dom_practice.js
+++ dom_practice.js
@@ -1,12 +1,26 @@
-document;
-document.firstElementChild;
-document.firstElementChild.firstElementChild;
-document.firstElementChild.lastElementChild;
-document.firstElementChild.lastElementChild.firstElementChild;
-var heading = document.firstElementChild.lastElementChild.firstElementChild;
-heading.innerHTML = "Good Bye.";
-heading.style.color = "green";
-document.querySelector("input").click();
-var third = document.firstElementChild.lastElementChild.lastElementChild.lastElementChild;
-third.innerHTML = "Usama";
-document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Usama";
+// document;
+// document.firstElementChild;
+// document.firstElementChild.firstElementChild;
+// document.firstElementChild.lastElementChild;
+// document.firstElementChild.lastElementChild.firstElementChild;
+// var heading = document.firstElementChild.lastElementChild.firstElementChild;
+// heading.innerHTML = "Good Bye.";
+// heading.style.color = "green";
+// document.querySelector("input").click();
+// var third = document.firstElementChild.lastElementChild.lastElementChild.lastElementChild;
+// third.innerHTML = "Usama";
+// document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Usama";
+// document.getElementsByTagName("li");
+// document.getElementsByTagName("li").style.color = "green"; // Error.
+// document.getElementsByTagName("li")[2].style.color = "green";
+// document.getElementsByTagName("li").length;
+// document.getElementsByClassName("btn");
+// document.getElementsByClassName("btn").style.color = "green"; // Error.
+// document.getElementsByClassName("btn")[0].style.color = "green";
+// document.getElementById("title");
+// document.getElementById("title").innerHTML = "Good Bye.";
+// document.querySelector("h1");
+// document.querySelector("#title");
+// document.querySelector(".btn");
+// document.querySelector("li a");
+// document.querySelector("a");
