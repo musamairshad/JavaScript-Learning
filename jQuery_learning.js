c84e201f530e71aa75a1ff2921df9826c1ccd352
Usama Irshad <imusamairshad@gmail.com>
2024-04-08 03:19:03 -0700

    added.

---
+++ jQuery_learning.js
@@ -0,0 +1,19 @@
+// ...jQuery Learning...
+
+// alert("Welcome!");
+// $("h1").css("color", "red");
+
+// $(document).ready(function() {
+//     $("h1").css("color", "red");
+// });
+
+// $("h1");
+// $("button");
+// $("h1").css("color", "green");
+// $("h1").css("color");
+// $("h1").css("font-size");
+// $("h1").css("font-size", "5rem");
+// $("h1").addClass("big-title");
+// $("h1").removeClass("big-title");
+// $("h1").hasClass("big-title");
+// $("h1").addClass("big-title margin-50");
