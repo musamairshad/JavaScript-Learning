76d4a13a3bfbf851096db6a4b6aac91ac52d10e6
Usama Irshad <imusamairshad@gmail.com>
2024-04-09 02:53:02 -0700

    modified.

--- jQuery_learning.js
+++ jQuery_learning.js
@@ -66,3 +66,22 @@
 // $("h1").prepend("<button>Prepend</button");
 // $("h1").append("<button>Append</button");
 // $("button").remove();
+
+// $("button").on("click", function() {
+    // $("h1").hide();
+    // $("h1").show();
+    // $("h1").toggle();
+    // $("h1").fadeOut();
+    // $("h1").fadeIn();
+    // $("h1").fadeToggle();
+    // $("h1").slideUp();
+    // $("h1").slideDown();
+    // $("h1").slideToggle();
+    // $("h1").animate({
+        // opacity: 0.5,
+        // opacity: "50%",
+    // });
+    // $("h1").slideUp().slideDown().animate({
+        // opacity: 0.5,
+    // });
+// });
