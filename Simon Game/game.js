095f727a40e1e65743d0b0942b56e287a9c0f94a
Usama Irshad <imusamairshad@gmail.com>
2024-05-03 11:38:58 -0700

    modified.

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -5,4 +5,8 @@ function nextSequence() {
     randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber]
     gamePattern.push(randomChosenColor);
+    var selectedButton = $("#" + randomChosenColor);
+    selectedButton.fadeOut(100).fadeIn(100);
+    var sound = new Audio("sounds/" + randomChosenColor + ".mp3");
+    sound.play();
 }
