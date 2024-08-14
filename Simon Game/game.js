1789a412d75251725abdb12b92522e68c57c582a
Usama Irshad <imusamairshad@gmail.com>
2024-08-14 22:20:03 +0500

    modify game.js

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -1,1 +1,10 @@
-alert("Hello, World");
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function nextSequence() {
+    var randomNumber = Math.random();
+    randomNumber = randomNumber * 4;
+    randomNumber = Math.floor(randomNumber);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+}
