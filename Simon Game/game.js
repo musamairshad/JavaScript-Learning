bef48ecf62d33688db0d716a3182bbecce44e415
Usama Irshad <imusamairshad@gmail.com>
2024-08-25 21:57:43 +0500

    modify game.js

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -2,10 +2,16 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
+function playSound(name) {
+    var selectedButtonSound = new Audio("sounds/" + name + ".mp3");
+    selectedButtonSound.play();
+}
+
 $(".btn").on("click", function () {
     // var userChosenColour = $("#" + randomChosenColor).attr("id");
     // OR
     var userChosenColour = $(this).attr("id");
+    playSound(userChosenColour);
     userClickedPattern.push(userChosenColour);
     console.log(userClickedPattern);
 });
@@ -18,6 +24,5 @@ function nextSequence() {
     gamePattern.push(randomChosenColor);
     var selectedButton = $("#" + randomChosenColor);
     selectedButton.fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    var selectedButtonSound = new Audio("sounds/" + randomChosenColor + ".mp3");
-    selectedButtonSound.play();
+    playSound(randomChosenColor);
 }
